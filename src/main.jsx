453ea56a78f39e8d68f41a1717b70638c5589c15
LYZAcell <applelgy@naver.com>
2024-07-28 16:39:40 +0900

    중커톤용 지도  api 사용해보기

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,63 @@
-// src/main.jsx
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import "./index.css";
-import Root from "./routes/root";
-import ErrorPage from "./error-page";
+import React, { useEffect, useState } from 'react';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage/>,
-  },
-]);
+const App = () => {
+  const [map, setMap] = useState(null);
+  const [places, setPlaces] = useState([]);
+  const [keyword, setKeyword] = useState('병원');
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+  useEffect(() => {
+    const container = document.getElementById('map');
+    const options = {
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3
+    };
+    const map = new window.kakao.maps.Map(container, options);
+    setMap(map);
+  }, []);
+
+  const searchPlaces = () => {
+    const ps = new window.kakao.maps.services.Places();
+    ps.keywordSearch(keyword, (data, status) => {
+      if (status === window.kakao.maps.services.Status.OK) {
+        setPlaces(data);
+        displayMarkers(data);
+      }
+    });
+  };
+
+  const displayMarkers = (places) => {
+    const bounds = new window.kakao.maps.LatLngBounds();
+    places.forEach(place => {
+      const markerPosition = new window.kakao.maps.LatLng(place.y, place.x);
+      const marker = new window.kakao.maps.Marker({
+        position: markerPosition,
+        map: map
+      });
+      bounds.extend(markerPosition);
+    });
+    map.setBounds(bounds);
+  };
+
+  return (
+    <div>
+      <h1>Kakao Map Hospital Search</h1>
+      <input
+        type="text"
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+      />
+      <button onClick={searchPlaces}>Search</button>
+      <div id="map" style={{ width: '100%', height: '400px' }}></div>
+      <ul>
+        {places.map((place, index) => (
+          <li key={index}>
+            <h2>{place.place_name}</h2>
+            <p>{place.road_address_name || place.address_name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
